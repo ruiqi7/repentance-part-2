83c696d97e9699e7d4fbf6f037263aba34219310
Tin Ruiqi <tinruiqi2002@gmail.com>
2023-09-19 14:01:30 +1000

    Place candle in front of tombstone

--- Assets/Scripts/ItemScripts/CandleHandler.cs
+++ Assets/Scripts/ItemScripts/CandleHandler.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CandleHandler : ItemHandlerInterface
 {
+    [SerializeField] private int interactDistance = 2;
+    
     public override bool HandleBehavior()
     {
-        GameObject player = GameObject.FindWithTag("Player");
-        Vector3 playerPos = player.transform.position;
-        Vector3 playerDir = player.transform.forward;
-        Vector3 pos = playerPos + playerDir;
-        pos.y = 0.0f;
-        Quaternion playerRot = player.transform.rotation;
-        Instantiate(gameObject, pos, playerRot * Quaternion.Euler(0.0f, 180.0f, 0.0f));
-        return true;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, interactDistance)) {
+            if (hit.collider.CompareTag("Tombstone")) {
+                PlaceCandle(hit.collider.gameObject);
+                return true;
+            }
+        }
+        // show some UI text
+        return false;
+    }
+
+    private void PlaceCandle(GameObject tombstone)
+    {
+        Vector3 tombstonePos = tombstone.transform.position;
+        Vector3 displacement = new Vector3(0.15f, 0.0f, -0.5f);
+        Vector3 pos = tombstonePos + displacement;
+        Instantiate(gameObject, pos, Quaternion.identity);
     }
 }
