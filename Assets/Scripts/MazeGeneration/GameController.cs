c8afda99420bacb2d451388eb6b24d6f7af51999
ElyseTan <140373855+ElyseTan@users.noreply.github.com>
2023-09-21 18:18:27 +1000

    Basic Maze Generation Code Added

---
+++ Assets/Scripts/MazeGeneration/GameController.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]               // 1
+
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor generator;
+
+    void Start()
+    {
+        generator = GetComponent<MazeConstructor>();      // 2
+        generator.GenerateNewMaze(13, 15);
+    }
+}
+
---
+++ Assets/Scripts/MazeGeneration/MazeConstructor.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class MazeConstructor : MonoBehaviour
+{
+    //1
+    public bool showDebug;
+    
+    [SerializeField] private Material mazeMat1;
+    [SerializeField] private Material mazeMat2;
+    [SerializeField] private Material startMat;
+
+    private MazeDataGenerator dataGenerator;
+    private MazeMeshGenerator meshGenerator;
+
+
+    //2
+    public int[,] data
+    {
+        get; private set;
+    }
+
+    //3
+    void Awake()
+    {
+        meshGenerator = new MazeMeshGenerator();
+        dataGenerator = new MazeDataGenerator();
+        // default to walls surrounding a single empty cell
+        data = new int[,]
+        {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+    }
+    
+    public void GenerateNewMaze(int sizeRows, int sizeCols)
+    {
+           if (sizeRows % 2 == 0 && sizeCols % 2 == 0)
+    {
+        Debug.LogError("Odd numbers work better for dungeon size.");
+    }
+
+    data = dataGenerator.FromDimensions(sizeRows, sizeCols);
+    DisplayMaze();
+    }
+
+    private void DisplayMaze()
+{
+    GameObject go = new GameObject();
+    go.transform.position = Vector3.zero;
+    go.name = "Procedural Maze";
+    go.tag = "Generated";
+
+    MeshFilter mf = go.AddComponent<MeshFilter>();
+    mf.mesh = meshGenerator.FromData(data);
+    
+    MeshCollider mc = go.AddComponent<MeshCollider>();
+    mc.sharedMesh = mf.mesh;
+
+    MeshRenderer mr = go.AddComponent<MeshRenderer>();
+    mr.materials = new Material[2] {mazeMat1, mazeMat2};
+}
+
+
+    void OnGUI()
+    {
+        //1
+        if (!showDebug)
+        {
+        return;
+    }
+
+    //2
+    int[,] maze = data;
+    int rMax = maze.GetUpperBound(0);
+    int cMax = maze.GetUpperBound(1);
+
+    string msg = "";
+
+    //3
+    for (int i = rMax; i >= 0; i--)
+    {
+        for (int j = 0; j <= cMax; j++)
+        {
+            if (maze[i, j] == 0)
+            {
+                msg += "....";
+            }
+            else
+            {
+                msg += "==";
+            }
+        }
+        msg += "\n";
+    }
+
+    //4
+    GUI.Label(new Rect(20, 20, 500, 500), msg);
+}
+
+}
