6b30b2e94805dbe554f16948bec44f9d24832110
Tin Ruiqi <tinruiqi2002@gmail.com>
2023-09-28 15:59:10 +1000

    Add eyeball item

---
+++ Assets/Scripts/ItemScripts/EyeballsHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeballsHandler : ItemHandlerInterface
+{
+    public override bool HandleBehavior()
+    {
+        ShowMonologue(false);
+        return false;
+    }
+}
