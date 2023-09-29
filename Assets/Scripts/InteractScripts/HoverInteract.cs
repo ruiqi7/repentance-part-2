b518ec17c5b92c3f216bd8c5f3ead14c4f29f56e
vk-comp <vkhuat@student.unimelb.edu.au>
2023-09-29 19:34:42 +1000

    Particle Interaction

--- Assets/Scripts/InteractScripts/HoverInteract.cs
+++ Assets/Scripts/InteractScripts/HoverInteract.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class HoverInteract : InteractableInterface
 {
+    public Transform Object;
     public override void interact(){
+        if(this.particle){
+            ParticleSystem part = Instantiate(this.particle, Object.transform);
+            part.Play();
+        }
     }
 }
--- Assets/Scripts/InteractableInterface.cs
+++ Assets/Scripts/InteractableInterface.cs
@@ -6,6 +6,7 @@ public abstract class InteractableInterface : MonoBehaviour
 {
     public string interactText;
     [SerializeField] public TMP_FontAsset font;
+    [SerializeField] public ParticleSystem particle;
     public virtual void interact() {}
 
 }
